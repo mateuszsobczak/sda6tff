6584d7fa7d7518e8838a5ccc804775eda514d4b1
Mateusz Sobczak <sobczak.mat@gmail.com>
2018-02-24 11:24:20 +0100

    capitalised named functionalisty

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -1,9 +1,18 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class Writer {
     public String sayHello(String name) {
+
         String nameInMessage = name;
+        String greeting = "Hello, ";
+        //String output = greeting + nameInMessage + "!";
+        if (StringUtils.isAllUpperCase(nameInMessage)) {
+            greeting = StringUtils.upperCase(greeting);
+        }
         if (nameInMessage == null || "".equals(nameInMessage)) { //nameInMessage.equals("") w tym przypadku nullPointerExeption moze poleciec
             nameInMessage = "my friend";
         }
-        return "Hello, " + nameInMessage + "!";
+
+        return greeting + nameInMessage + "!";
     }
 }
--- src/test/java/WriterTest.java
+++ src/test/java/WriterTest.java
@@ -1,10 +1,16 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WriterTest {
+    private Writer writer;
+    @Before
+    public void init(){
+        writer = new Writer();
+    }
     @Test
     public void sayHelloWithGivenNameTest() {
-        Writer writer = new Writer();
+        //Writer writer = new Writer();
         String name = "Szymon";
         String result = writer.sayHello(name);
 
@@ -13,7 +19,7 @@ public class WriterTest {
 
     @Test
     public void sayHelloWithNoNameSpecified() {
-        Writer writer = new Writer();
+        //Writer writer = new Writer();
         String name = "";
         String result = writer.sayHello("");
 
@@ -23,10 +29,18 @@ public class WriterTest {
 
     @Test
     public void sayHelloWithNullName() {
-        Writer writer = new Writer();
+        //Writer writer = new Writer();
         String name = null;
 
         String result = writer.sayHello(name);
         Assert.assertEquals("Hello, my friend!", result);
+    }
+    @Test
+    public void sayHelloWithUpperCasedName(){
+        //Writer writer = new Writer();
+        String name = "SZYMON";
+
+        String result = writer.sayHello(name);
+        Assert.assertEquals("HELLO, SZYMON!", result);
     }
 }
